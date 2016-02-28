e58e0c2ac9a0cde1941f3cbea2d21ac13b4eac5b
namrathashivashankar <namratha.shivashankar@mavs.uta.edu>
2016-02-28 03:53:56 -0600

    Update STRINGRE.C

--- STRINGRE.C
+++ STRINGRE.C
@@ -1,28 +1,31 @@
+/* Code to Reverse a given String using the ASCII values of the Characters in the String*/
+
 #include<stdio.h>
 #include<conio.h>
 
 void stringrev(char* str){
-int len = strlen(str);
-int i = 0;
-int a,b,c;
-printf("%d",len);
-while(i<len/2){
-a = str[i];
-b = str[len-i-1];
-c = a-b;
-
-a-=c;
-b+=c;
-str[i] =(char)a;
-str[len-i-1] = (char)b;
-i++;
-}
-printf("%s",str);
+  int len = strlen(str);
+  int i = 0;
+  int a,b,c;
+  while(i<len/2){
+    //Get the ASCII values of the characters
+    a = str[i];
+    b = str[len-i-1];
+    //Swap the values
+    c = a-b;
+    a-=c;
+    b+=c;
+    str[i] =(char)a;
+    str[len-i-1] = (char)b;
+    i++;
+  }
+  printf("%s",str);
+  
 }
 
 void main(){
-char str[] = "ganapa%";
-clrscr();
-stringrev(str);
-getch();
+  char str[] = "Namratha";
+  clrscr();
+  stringrev(str);
+  getch();
 }
